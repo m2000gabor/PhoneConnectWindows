3bdf7da61888e0fd071951bdb60554c0c831608e
Gabor <sbzbxr@inf.elte.hu>
2022-03-08 11:18:57 +0100

    Fixed bug: multiple TrayIcon added to Windows HiddenTrayIcons.

--- src/hu/elte/sbzbxr/controller/Controller.java
+++ src/hu/elte/sbzbxr/controller/Controller.java
@@ -82,21 +82,22 @@ public class Controller {
         }
     }
 
-    public static void displayTray(SendableNotification notification) throws AWTException {
-        //Obtain only one instance of the SystemTray object
-        SystemTray tray = SystemTray.getSystemTray();
-
-        //If the icon is a file
-        Image image = Toolkit.getDefaultToolkit().createImage("resources/icon.jpg");
-        //Alternative (if the icon is on the classpath):
-        //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
-
-        TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
-        //Let the system resize the image if needed
-        trayIcon.setImageAutoSize(true);
-        //Set tooltip text for the tray icon
-        trayIcon.setToolTip("System tray icon demo");
-        tray.add(trayIcon);
+    private TrayIcon trayIcon = null;
+    public void displayTray(SendableNotification notification) throws AWTException {
+        if(trayIcon == null){
+            //Obtain only one instance of the SystemTray object
+            SystemTray tray = SystemTray.getSystemTray();
+            //If the icon is a file
+            Image image = Toolkit.getDefaultToolkit().createImage("resources/icon.jpg");
+            //Alternative (if the icon is on the classpath):
+            //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
+            trayIcon = new TrayIcon(image, "Tray Demo");
+            //Let the system resize the image if needed
+            trayIcon.setImageAutoSize(true);
+            //Set tooltip text for the tray icon
+            trayIcon.setToolTip("System tray icon demo");
+            tray.add(trayIcon);
+        }
 
         trayIcon.displayMessage(notification.getTitle(), notification.getText(), TrayIcon.MessageType.INFO);
     }
