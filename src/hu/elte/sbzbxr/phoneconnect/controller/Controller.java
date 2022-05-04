2f1cf34f828701376de394dde2061a998a127890
Gabor <sbzbxr@inf.elte.hu>
2022-05-04 14:08:50 +0200

    Fixed JFrame showed, immediately disposed, showed again bug.

--- src/hu/elte/sbzbxr/phoneconnect/controller/Controller.java
+++ src/hu/elte/sbzbxr/phoneconnect/controller/Controller.java
@@ -127,6 +127,7 @@ public class Controller {
     }
 
     public void endOfStreaming() {
+        if(frameScreenShare==null) return;
         System.err.println("End of streaming");
         disposeAll();
         pictureProvider.stop();
